80e54073c4b60b74ce9992f11299bb6b45cd94fc
Vikram Singh <vikzilla18@gmail.com>
2020-05-19 17:44:33 -0400

    Created unit testing cmake commands.

---
+++ tests/SimpleTest.cpp
@@ -0,0 +1,5 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(passing_test){
+    BOOST_CHECK(1 == 2);
+}
---
+++ tests/main.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE "V6502 Unit Tests"
+#include <boost/test/included/unit_test.hpp>
+#include <V6502/CPU.h>
