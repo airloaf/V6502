2646d0f896d85721a33cc286b54e472c81bc4b18
Vikram Singh <vikzilla18@gmail.com>
2021-01-22 18:58:11 -0500

    Corrected two of the transfer unit tests. Implemented all the transfer instructions. #11

--- src/V6502/Instructions/InstructionSet.cpp
+++ src/V6502/Instructions/InstructionSet.cpp
@@ -8,26 +8,32 @@ namespace V6502
         // Transfer instructions        
         void TAX(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
+            rf.indexX = rf.accumulator;
         }
 
         void TAY(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
+            rf.indexY = rf.accumulator;
         }
 
         void TSX(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
+            rf.indexX = rf.stackPointer;
         }
 
         void TXA(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
+            rf.accumulator = rf.indexX;
         }
 
         void TXS(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
+            rf.stackPointer = rf.indexX;
         }
 
         void TYA(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
+            rf.accumulator = rf.indexY;
         }
 
     } // namespace InstructionSet
