a675225ed01ae8ee4a002110aac8f16b030f3f0f
Vikram Singh <vikzilla18@gmail.com>
2021-01-23 17:26:52 -0500

    Wrote test for Immediate mode addressing #11

--- tests/AddressingModes.cpp
+++ tests/AddressingModes.cpp
@@ -24,4 +24,14 @@ BOOST_AUTO_TEST_CASE(IMPLIED_TEST){
     BOOST_CHECK_EQUAL(implied(rf, nullptr, decoded, 0), true);
 }
 
+static auto IMM_PC = bdata::make({0x0000, 0x1234, 0xDEAD, 0xBEEF});
+BOOST_DATA_TEST_CASE_F(Fixture, IMMEDIATE_TEST, IMM_PC, pc)
+{
+    rf.programCounter = pc;
+    uint16_t decoded;
+
+    BOOST_CHECK_EQUAL(implied(rf, bus, decoded, 0), true);
+    BOOST_CHECK_EQUAL(decoded, pc+1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
