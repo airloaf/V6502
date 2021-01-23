d7287675c75b452fa460f74fc8ec6f33be73b5d9
Vikram Singh <vikzilla18@gmail.com>
2021-01-23 11:23:22 -0500

    Corrected tests for stack instructions. Implemented stack instructions #11

--- src/V6502/Instructions/InstructionSet/Stack.cpp
+++ src/V6502/Instructions/InstructionSet/Stack.cpp
@@ -6,22 +6,30 @@ namespace V6502
     {
         void PHA(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
-
+            uint16_t addr = (uint8_t) rf.stackPointer + 0x100;
+            bus->write(addr, rf.accumulator);
+            rf.stackPointer--;
         }
 
         void PHP(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
-
+            uint16_t addr = (uint8_t) rf.stackPointer + 0x100;
+            bus->write(addr, rf.status);
+            rf.stackPointer--;
         }
 
         void PLA(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
-
+            rf.stackPointer++;
+            uint16_t addr = (uint8_t) rf.stackPointer + 0x100;
+            rf.accumulator = bus->read(addr);
         }
 
         void PLP(RegisterFile &rf, MemoryBus *bus, uint16_t decoded, int cycle)
         {
-
+            rf.stackPointer++;
+            uint16_t addr = (uint8_t) rf.stackPointer + 0x100;
+            rf.status = bus->read(addr);
         }
 
     } // namespace InstructionSet
--- tests/InstructionSet/Stack.cpp
+++ tests/InstructionSet/Stack.cpp
@@ -15,40 +15,43 @@ BOOST_AUTO_TEST_SUITE(STACK_INSTRUCTIONS)
 static auto PHA_VALUE = bdata::make({0x00, 0x12, 0xFF});
 static auto PHA_STACK = bdata::make({0x00, 0xFF, 0x77});
 static auto PHA_S_AFT = bdata::make({0xFF, 0xFE, 0x76});
-static auto PHA_DATA = PHA_VALUE ^ PHA_STACK ^ PHA_S_AFT;
-BOOST_DATA_TEST_CASE_F(Fixture, PHA_TEST, PHA_DATA, v, sp, sp_after)
+static auto PHA_ADDR = bdata::make({0x100, 0x1FF, 0x177});
+static auto PHA_DATA = PHA_VALUE ^ PHA_STACK ^ PHA_S_AFT ^ PHA_ADDR;
+BOOST_DATA_TEST_CASE_F(Fixture, PHA_TEST, PHA_DATA, v, sp, sp_after, addr)
 {
     rf.accumulator = v;
     rf.stackPointer = sp;
 
     PHA(rf, bus, 0, 0);
 
-    BOOST_CHECK_EQUAL(bus->read(sp), v);
+    BOOST_CHECK_EQUAL(bus->read(addr), v);
     BOOST_CHECK_EQUAL(rf.stackPointer, sp_after);
 }
 
 static auto PHP_VALUE = bdata::make({0x00, 0x12, 0xFF});
 static auto PHP_STACK = bdata::make({0x00, 0xFF, 0x77});
 static auto PHP_S_AFT = bdata::make({0xFF, 0xFE, 0x76});
-static auto PHP_DATA = PHP_VALUE ^ PHP_STACK ^ PHP_S_AFT;
-BOOST_DATA_TEST_CASE_F(Fixture, PHP_TEST, PHP_DATA, v, sp, sp_after)
+static auto PHP_ADDR = bdata::make({0x100, 0x1FF, 0x177});
+static auto PHP_DATA = PHP_VALUE ^ PHP_STACK ^ PHP_S_AFT ^ PHP_ADDR;
+BOOST_DATA_TEST_CASE_F(Fixture, PHP_TEST, PHP_DATA, v, sp, sp_after, addr)
 {
     rf.status = v;
     rf.stackPointer = sp;
 
     PHP(rf, bus, 0, 0);
 
-    BOOST_CHECK_EQUAL(bus->read(sp), v);
+    BOOST_CHECK_EQUAL(bus->read(addr), v);
     BOOST_CHECK_EQUAL(rf.stackPointer, sp_after);
 }
 
 static auto PLA_VALUE = bdata::make({0x00, 0x12, 0xFF});
 static auto PLA_STACK = bdata::make({0x00, 0xFF, 0x77});
 static auto PLA_S_AFT = bdata::make({0x01, 0x00, 0x78});
-static auto PLA_DATA = PLA_VALUE ^ PLA_STACK ^ PLA_S_AFT;
-BOOST_DATA_TEST_CASE_F(Fixture, PLA_TEST, PLA_DATA, v, sp, sp_after)
+static auto PLA_ADDR = bdata::make({0x101, 0x100, 0x178});
+static auto PLA_DATA = PLA_VALUE ^ PLA_STACK ^ PLA_S_AFT ^ PLA_ADDR;
+BOOST_DATA_TEST_CASE_F(Fixture, PLA_TEST, PLA_DATA, v, sp, sp_after, addr)
 {
-    bus->write(sp_after, v);
+    bus->write(addr, v);
     rf.stackPointer = sp;
 
     PLA(rf, bus, 0, 0);
@@ -60,10 +63,11 @@ BOOST_DATA_TEST_CASE_F(Fixture, PLA_TEST, PLA_DATA, v, sp, sp_after)
 static auto PLP_VALUE = bdata::make({0x00, 0x12, 0xFF});
 static auto PLP_STACK = bdata::make({0x00, 0xFF, 0x77});
 static auto PLP_S_AFT = bdata::make({0x01, 0x00, 0x78});
-static auto PLP_DATA = PLP_VALUE ^ PLP_STACK ^ PLP_S_AFT;
-BOOST_DATA_TEST_CASE_F(Fixture, PLP_TEST, PLP_DATA, v, sp, sp_after)
+static auto PLP_ADDR = bdata::make({0x101, 0x100, 0x178});
+static auto PLP_DATA = PLP_VALUE ^ PLP_STACK ^ PLP_S_AFT ^ PLP_ADDR;
+BOOST_DATA_TEST_CASE_F(Fixture, PLP_TEST, PLP_DATA, v, sp, sp_after, addr)
 {
-    bus->write(sp_after, v);
+    bus->write(addr, v);
     rf.stackPointer = sp;
 
     PLP(rf, bus, 0, 0);
